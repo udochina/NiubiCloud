a7a94ffc41d1e105f3107b9232e62371d727dd2e
恋空 <2928455383@qq.com>
2020-07-29 19:24:11 +0800

    删除文件 src/com/uxcontry/geekserver/plugin

--- src/com/uxcontry/geekserver/plugin/PluginManager.java
+++
@@ -1,8 +0,0 @@
-package com.uxcontry.geekserver.plugin;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class PluginManager {
-	public static final List<Plugin> plugin = new ArrayList<Plugin>();
-}
